934987e13efb97e2f230655605f400d7674715f8
Jimmy Tran <jimmtra@amazon.com>
2020-06-19 16:15:34 -0700

    Init commit after 'cfn init' default generator

---
+++ aws-codegurureviewer-repository/src/main/java/software/amazon/codegurureviewer/repository/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.codegurureviewer.repository;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-codegurureviewer-repository.json");
+    }
+}
