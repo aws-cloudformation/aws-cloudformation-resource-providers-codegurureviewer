91a57e60fbea6eeaad29d8b7f98e0d54b7fa54d5
jimmyyentran <jimmtra@amazon.com>
2020-06-24 16:36:08 -0700

    Refactor Repository resource to RepositoryAssociation (#4)
    * Refactor Repository resource to RepositoryAssociation
    
    * Force Type as a required property
    
    * Update the json SAM tests
    
    * Remove original Initializing new project
    Found existing settings - overwrite (y/N)?
    AWS::CodeGuruReviewer::RepositoryAssociation
    >>
    aborted boostrap
    
    * Update callGraph strings to RepositoryAssociation

---
+++ aws-codegurureviewer-repositoryassociation/src/main/java/software/amazon/codegurureviewer/repositoryassociation/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.codegurureviewer.repositoryassociation;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-codegurureviewer-repository.json");
+    }
+}
---
+++ aws-codegurureviewer-repositoryassociation/src/main/java/software/amazon/codegurureviewer/repositoryassociation/Translator.java
@@ -0,0 +1,79 @@
+package software.amazon.codegurureviewer.repositoryassociation;
+
+import software.amazon.awssdk.services.codegurureviewer.model.AssociateRepositoryRequest;
+import software.amazon.awssdk.services.codegurureviewer.model.AssociateRepositoryResponse;
+import software.amazon.awssdk.services.codegurureviewer.model.CodeCommitRepository;
+import software.amazon.awssdk.services.codegurureviewer.model.DescribeRepositoryAssociationRequest;
+import software.amazon.awssdk.services.codegurureviewer.model.DescribeRepositoryAssociationResponse;
+import software.amazon.awssdk.services.codegurureviewer.model.DisassociateRepositoryRequest;
+import software.amazon.awssdk.services.codegurureviewer.model.ProviderType;
+import software.amazon.awssdk.services.codegurureviewer.model.Repository;
+import software.amazon.awssdk.services.codegurureviewer.model.ThirdPartySourceRepository;
+import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
+
+/**
+ * This class is a centralized placeholder for
+ * - api request construction
+ * - object translation to/from aws sdk
+ * - resource model construction for read/list handlers
+ */
+
+public final class Translator {
+
+    /**
+     * Request to create a resource
+     *
+     * @param model resource model
+     * @return awsRequest the aws service request to create a resource
+     */
+    static AssociateRepositoryRequest translateToAssociateRepositoryRequest(final ResourceModel model) {
+        Repository repository = null;
+        String providerType = model.getType();
+        if (providerType.equals(ProviderType.CODE_COMMIT.toString())) {
+            repository = Repository.builder().codeCommit(
+                    CodeCommitRepository.builder()
+                            .name(model.getName())
+                            .build()
+            ).build();
+        } else if (providerType.equals(ProviderType.BITBUCKET.toString())) {
+            repository = Repository.builder().bitbucket(
+                    ThirdPartySourceRepository.builder()
+                            .name(model.getName())
+                            .connectionArn(model.getConnectionArn())
+                            .owner(model.getOwner())
+                            .build()
+            ).build();
+        } else {
+            throw new CfnInvalidRequestException(String.format("Unknown ProviderType of %s", model.getType()));
+        }
+
+        return AssociateRepositoryRequest.builder().repository(repository).build();
+    }
+
+    static DescribeRepositoryAssociationRequest translateToDescribeRepositoryAssociationRequest(final AssociateRepositoryResponse associateRepositoryResponse) {
+        return DescribeRepositoryAssociationRequest.builder()
+                .associationArn(associateRepositoryResponse.repositoryAssociation().associationArn()).build();
+    }
+
+    static DescribeRepositoryAssociationRequest translateToDescribeRepositoryAssociationRequest(final ResourceModel model) {
+        return DescribeRepositoryAssociationRequest.builder()
+                .associationArn(model.getAssociationArn()).build();
+    }
+
+    static ResourceModel translateFromReadResponse(final DescribeRepositoryAssociationResponse awsResponse) {
+        return ResourceModel.builder()
+                .associationArn(awsResponse.repositoryAssociation().associationArn())
+                .name(awsResponse.repositoryAssociation().name())
+                .type(awsResponse.repositoryAssociation().providerType().toString())
+                .owner(awsResponse.repositoryAssociation().owner())
+                .connectionArn(awsResponse.repositoryAssociation().connectionArn())
+                .state(awsResponse.repositoryAssociation().state().toString())
+                .build();
+    }
+
+    static DisassociateRepositoryRequest translateToDisassociateRepositoryRequest(final ResourceModel model) {
+        return DisassociateRepositoryRequest.builder()
+                .associationArn(model.getAssociationArn())
+                .build();
+    }
+}
